5dbc21c71112beef952f05c8556fd99960c1f670
emilio <emiliosanchez0611@gmail.com>
2026-02-26 07:56:16 -0500

    agregue el segundo ejercicio resuelto

---
+++ 02_par_impar.py
@@ -0,0 +1,10 @@
+num =int(input("Ingresa un numero entero "))
+if num%2 == 0:
+	print("El numero es par")
+else:
+	print("El numero es impar")
+edad =int(input("Ingresa tu edad "))
+if edad >=18:
+	print("Es mayor de edad")
+else:
+	print("Es menor de edad")
